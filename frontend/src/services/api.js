33437093c792bf235e1a929a0e0b42abe7a92dd2
Vanshika Raina <vanshika8727@gmail.com>
2025-12-24 14:27:59 +0530

    remove frontend folder

--- frontend/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  // baseURL: import.meta.env.VITE_API_BASE_URL,
-  baseURL:"http://localhost:5000/api",
-  withCredentials: true,
-});
-
-export default api;
